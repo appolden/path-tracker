a000a60e1850509f6671f4db1ce03e55b9e51682
a.polden@gmail.com <Al P>
2018-03-29 20:50:25 +0100

    Add current location to list of points of interest

--- src/component/path-tracker.jsx
+++ src/component/path-tracker.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { Component } from 'react';
 import MapHelper from '../maps/map-helper.js';
 import PointOfInterest from '../component/point-of-interest.jsx';
-//const cache = na
+import PointCurrent from '../component/point-current.jsx';
 
 class PathTracker extends Component {
   constructor(props) {
@@ -14,12 +14,11 @@ class PathTracker extends Component {
     this.state = {
       lat: 42.965442,
       lng: -0.779819,
-      distances: [],
+      pointsOfInterest: [],
       nearestMetreOfPath: 0,
       elevationAtNearestMetreOfPath: 0
     };
     this.pointsWithDistance = [];
-    this.currentLocation = undefined;
     this.pois = [];
   }
 
@@ -30,13 +29,9 @@ class PathTracker extends Component {
 
   pointsLoaded(data) {
     this.pointsWithDistance = data;
-  }
-
-  pointsLoaded(data) {
-    this.pointsWithDistance = data;
     if (this.pointsWithDistance.length > 0 && this.pois.length > 0) {
       this.setState({
-        distances: this.pois
+        pointsOfInterest: this.pois
       });
     }
   }
@@ -71,7 +66,7 @@ class PathTracker extends Component {
     this.pois = data;
     if (this.pointsWithDistance.length > 0 && this.pois.length > 0) {
       this.setState({
-        distances: this.pois
+        pointsOfInterest: this.pois
       });
     }
   }
@@ -103,11 +98,11 @@ class PathTracker extends Component {
   }
 
   findNearestPointToCurrentLocationAndUpdate() {
-    this.currentLocation = { lat: this.state.lat, lng: this.state.lng };
+    const currentLocation = { lat: this.state.lat, lng: this.state.lng };
 
     const nearestPointToCurrentLocation = MapHelper.findNearestPoint(
       this.pointsWithDistance,
-      this.currentLocation
+      currentLocation
     );
 
     this.setState({
@@ -133,20 +128,52 @@ class PathTracker extends Component {
   }
 
   render() {
-    const pointOfInterestRows = this.state.distances.map(x => (
-      <PointOfInterest
-        key={x.name}
-        name={x.name}
-        nearestMetreOfPath={x.nearestMetreOfPath}
-        elevationAtNearestMetreOfPath={x.elevation}
-        cumulativeAscentAtNearestMetreOfPath={x.cumulativeAscent}
-        cumulativeDescentAtNearestMetreOfPath={x.cumulativeDescent}
+    const rows = [];
+    const pointCurrent = (
+      <PointCurrent
+        key="1"
         pathMetre={this.state.nearestMetreOfPath}
         pathElevation={this.state.elevationAtNearestMetreOfPath}
-        pathCumulativeAscent={this.state.cumulativeAscentAtNearestMetreOfPath}
-        pathCumulativeDescent={this.state.cumulativeDescentAtNearestMetreOfPath}
       />
-    ));
+    );
+
+    this.state.pointsOfInterest.forEach((x, index) => {
+      const pointOfInterest = (
+        <PointOfInterest
+          key={x.name}
+          name={x.name}
+          nearestMetreOfPath={x.nearestMetreOfPath}
+          elevationAtNearestMetreOfPath={x.elevation}
+          cumulativeAscentAtNearestMetreOfPath={x.cumulativeAscent}
+          cumulativeDescentAtNearestMetreOfPath={x.cumulativeDescent}
+          pathMetre={this.state.nearestMetreOfPath}
+          pathElevation={this.state.elevationAtNearestMetreOfPath}
+          pathCumulativeAscent={this.state.cumulativeAscentAtNearestMetreOfPath}
+          pathCumulativeDescent={
+            this.state.cumulativeDescentAtNearestMetreOfPath
+          }
+        />
+      );
+
+      if (index == this.state.pointsOfInterest.length - 1) {
+        //reached the end
+        rows.push(pointOfInterest);
+        if (x.nearestMetreOfPath < this.state.nearestMetreOfPath) {
+          rows.push(pointCurrent);
+        }
+        return;
+      }
+
+      const nextPointOfInterest = this.state.pointsOfInterest[index + 1];
+      if (
+        x.nearestMetreOfPath <= this.state.nearestMetreOfPath &&
+        nextPointOfInterest.nearestMetreOfPath >= this.state.nearestMetreOfPath
+      ) {
+        rows.push(pointCurrent);
+      } else {
+        rows.push(pointOfInterest);
+      }
+    });
 
     return (
       <div>
@@ -167,9 +194,7 @@ class PathTracker extends Component {
           />
           <input type="button" value="Update" onClick={this.handleClick} />
         </p>
-        <p>You are at {(this.state.nearestMetreOfPath * 0.001).toFixed(2)}</p>
-
-        <div className="pointsOfInterest">{pointOfInterestRows}</div>
+        <div className="pointsOfInterest">{rows}</div>
       </div>
     );
   }
---
+++ src/component/point-current.jsx
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PointCurrent extends Component {
+  render() {
+    return (
+      <div key={this.props.name} className="pointCurrent">
+        <div style={{ textAlign: 'right' }}>East</div>
+        <div style={{ textAlign: 'center', fontSize: '1.5em' }}>
+          Km: {(this.props.pathMetre * 0.001).toFixed(2)}, Alt:{' '}
+          {this.props.pathElevation} metres{' '}
+        </div>
+        <div style={{ textAlign: 'left' }}>West</div>
+      </div>
+    );
+  }
+}
+
+PointCurrent.propTypes = {
+  pathMetre: PropTypes.number,
+  pathElevation: PropTypes.number
+};
+
+export default PointCurrent;
