0660d407631f021a631a7edd70667ff2d7bade13
a.polden@gmail.com <Al P>
2018-03-30 21:40:41 +0100

    Fix location bug

--- src/component/location.jsx
+++ src/component/location.jsx
@@ -7,24 +7,28 @@ class Location extends Component {
     this.state = {
       lat: 0,
       lng: 0,
-        error: undefined,
-        getCurrentPositionInProgress: false,
-        buttonText: 'Update location'
+      error: undefined,
+      getCurrentPositionInProgress: false,
+      buttonText: 'Update location'
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(event) {
-      if ('geolocation' in navigator) {
-
-          this.setState({ buttonText: 'locating', getCurrentPositionInProgress: true })
+    if ('geolocation' in navigator) {
+      this.setState({
+        buttonText: 'locating',
+        getCurrentPositionInProgress: true
+      });
       navigator.geolocation.getCurrentPosition(
         position => {
           this.setState({
             lat: position.coords.latitude,
             lng: position.coords.longitude,
-            error: ''
+            error: '',
+            getCurrentPositionInProgress: false,
+            buttonText: 'Update location'
           });
 
           if (this.props.onLocationChanged !== undefined) {
@@ -33,13 +37,18 @@ class Location extends Component {
         },
         error => {
           this.setState({
-              error: 'Unable to find location. Ensure you have location enabled.', buttonText: 'Update location', getCurrentPositionInProgress: false
+            error: 'Unable to find location. Ensure you have location enabled.',
+            buttonText: 'Update location',
+            getCurrentPositionInProgress: false
           });
         },
         { timeout: 15000 }
       );
-      } else {
-          this.setState({ buttonText: 'Update location', getCurrentPositionInProgress: false })
+    } else {
+      this.setState({
+        buttonText: 'Update location',
+        getCurrentPositionInProgress: false
+      });
       alert('Geo-location is not avaiable!!');
     }
   }
@@ -49,9 +58,10 @@ class Location extends Component {
       <p>
         {this.state.lat.toFixed(5)}, {this.state.lng.toFixed(6)}{' '}
         <input
-                type="button"
-                value={this.state.buttonText}
-                onClick={this.handleClick} disabled={this.state.getCurrentPositionInProgress}
+          type="button"
+          value={this.state.buttonText}
+          onClick={this.handleClick}
+          disabled={this.state.getCurrentPositionInProgress}
         />{' '}
         {this.state.error}
       </p>
